6c03458b4d18bd2692c97407191e7d79149b89f1
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-07-24 12:34:27 +0530

    Modified Polymorphism added more

--- OOP/Polymorphism/Shaapes.java
+++ OOP/Polymorphism/Shaapes.java
@@ -1,8 +1,15 @@
 public class Shaapes {
+
+    //If we put [final] before class declaretion it will give us error in every class
+    //where we have used the final class as extended class
+    // final public class Shaapes{}-> This is a non changable class u cant extend or
+    //change anything
+
     // Shaapes(){
 
-    // } //Example of super
+    // } //Example of [super()]
+
    void area(){
-    System.out.println("I am from Shapes class");
+     System.out.println("I am from Shapes class");
    }
 }
--- OOP/Polymorphism/Square.java
+++ OOP/Polymorphism/Square.java
@@ -4,7 +4,7 @@ public class Square extends Shaapes {
    }
 
     // Here we are doing Method overloading,same name of the methods but with different
-    //parameters so java will automatically chose the correct one 
+    //parameters/Return type/value so java will automatically chose the correct one 
    void area(int a){
     System.out.println("I am from Square class with a value of "+a);
    }
