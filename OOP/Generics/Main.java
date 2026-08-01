6c6321ad641eac7c10f87b60bbd5dd238c9edb3b
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-01 11:30:42 +0530

    Added Main class

---
+++ OOP/Generics/Main.java
@@ -0,0 +1,9 @@
+package Generics;
+
+public class Main {
+public static void main(String[] args) {
+    IndraList obj = new IndraList();
+    obj.add(10000);
+    System.out.println(obj.get(0));
+}
+}
