bf9068d1db58af65f3869eb030ee7028945cd4b7
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-02 11:26:16 +0530

    Basic Exception Handeling Example

---
+++ Exception Handeling/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    
+   public static void main(String[] args) {
+    try{
+    int a=10;
+    int b=0;
+    System.out.println(a/b);  //This code will give exception
+   }catch(Exception e){
+    System.out.println("Please check the entered values and do again");
+   }
+}
+}
