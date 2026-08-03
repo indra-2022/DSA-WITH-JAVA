f72c227e028a51b52578fb1e6ec779bd455c1030
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-03 10:28:12 +0530

    Added Comment

--- Exception Handeling/Introduction.java
+++ Exception Handeling/Introduction.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+
 public class Introduction {
    public static void main(String[] args) {
     try{
     int a=10;
     int b=0;
-    System.out.println(a/b);  //This code will give exception
+    System.out.println(a/b);
+      //This code will give exception
+      
    }catch(Exception e){
     System.out.println("Please check the entered values and do again");
+    //Now this will not give any exception while running
+    //This is just a basic example of Exception Hndeling
    }
 }
 }
