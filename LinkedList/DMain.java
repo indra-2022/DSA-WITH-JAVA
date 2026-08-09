93c9710005a3cbeb7f30456e5b817f97aff9589f
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-09 11:36:48 +0530

    Changed in some methods

--- LinkedList/DMain.java
+++ LinkedList/DMain.java
@@ -1,13 +1,17 @@
 package Linkedlist;
 
 public class DMain {
-public static void main(String[] args) {
-    Doubly list = new Doubly();
-    list.DInsertFirst(20);
-    list.DInsertFirst(10);
-    list.DInsertLast(30);
-    list.display();
-    list.DdeleteIndex(2);
-    list.display();
-}
+    public static void main(String[] args) {
+        Doubly list = new Doubly();
+        list.DInsertFirst(20);
+        list.DInsertFirst(10);
+        list.DInsertLast(30);
+        list.DInsertFirst(990);
+        list.DInsertFirst(210);
+        list.DInsertLast(320);
+        // list.display();
+        // list.DDeleteIndex(2);
+        list.display();
+        list.DisplayRev();
+    }
 }
--- LinkedList/Doubly.java
+++ LinkedList/Doubly.java
@@ -46,64 +46,75 @@ public class Doubly {
         size++;
     }
 
-public void InsertAtIndex(int val,int index){
-if (index==0) {
-  DInsertFirst(val);
-  return;
-}
+    public void InsertAtIndex(int val, int index) {
+        if (index == 0) {
+            DInsertFirst(val);
+            return;
+        }
 
-if (index==size) {
-  DInsertLast(val);
-  return;
-}
+        if (index == size) {
+            DInsertLast(val);
+            return;
+        }
 
- Node temp = head;
- for(int i=0;i<index-1;i++){
-    temp=temp.next;
- }
-  Node ex = temp.next;
-  Node node = new Node(val);
-  node.next=temp.next;
-  ex.prev=node;
-  temp.next=node;
-  node.prev=temp;
-  size++;
-}
-                        // DELETING PART
-
-// public void DdeleteIndex(int index){
-//     Node temp = head;
-//    for(int i=0;i<index-2;i++){
-//       temp=temp.next; //-> Pointing to previous element of the index
-//    }
-//    Node ex = temp.next; // Pointing to index
-//    Node nxt = ex.next;
-//    temp.next=ex.next;
-//    nxt.prev = temp;
-//    size--;
-//    System.out.println(ex.value+" Is deleted");
-// }
-
-public void DDeleteIndex(int index){
-    Node temp = head;
-    for(int i=1;i<index-1;i++){
-    temp=temp.next;
-   }
-   temp.next=temp.next.next;
-  temp.next.prev = temp;
-  size--;
-}
-
-
-public void display(){ 
-  Node temp = head;
-  while (temp!= null) {
-    System.out.print(temp.value+"->");
-    temp=temp.next;
-    if (temp==null) {
-      System.out.print("End");
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        Node ex = temp.next;
+        Node node = new Node(val);
+        node.next = temp.next;
+        ex.prev = node;
+        temp.next = node;
+        node.prev = temp;
+        size++;
     }
-  }
-  System.out.println();
-}
+    // DELETING PART
+
+    // public void DdeleteIndex(int index){
+    // Node temp = head;
+    // for(int i=0;i<index-2;i++){
+    // temp=temp.next; //-> Pointing to previous element of the index
+    // }
+    // Node ex = temp.next; // Pointing to index
+    // Node nxt = ex.next;
+    // temp.next=ex.next;
+    // nxt.prev = temp;
+    // size--;
+    // System.out.println(ex.value+" Is deleted");
+    // }
+
+    public void DDeleteIndex(int index) {
+        Node temp = head;
+        for (int i = 1; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        temp.next.prev = temp;
+        size--;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
+            if (temp == null) {
+                System.out.print("End");
+            }
+        }
+        System.out.println();
+    }
+
+    public void DisplayRev() {
+        Node temp = tail;
+        while (temp != null) {
+            System.out.print(temp.value + "<-");
+            temp = temp.prev;
+            if (temp == null) {
+                System.out.print("Start");
+            }
+        }
+        System.out.println();
+    }
 }
