0ef49635cac0969ba15e1b26eadccd8c4b49fcbc
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-14 11:55:07 +0530

    Created main class 1

---
+++ LinkedListQS/Main.java
@@ -0,0 +1,18 @@
+package LinkedListQS;
+
+public class Main {
+    public static void main(String[] args) {
+        // MergeSorted list1 = new MergeSorted();
+        // list1.InsertFirst(10);
+        // list1.InsertFirst(9);
+        // list1.InsertFirst(8);
+        // list1.display();  //UNCOMMENT FOR MERGE BOTH SORTED LINKEDLISTS
+        // MergeSorted list2 = new MergeSorted();
+        // list2.InsertFirst(90);
+        // list2.InsertFirst(80);
+        // list2.InsertFirst(70);
+        // list2.display();
+        // MergeSorted.Merge(list1,list2);
+        
+    }
+}
