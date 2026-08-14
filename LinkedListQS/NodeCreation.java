d15e45937069b2c6a7b52a26c2b8732f76caf9ff
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-14 11:54:18 +0530

    Changed the NodeCreation class

--- LinkedListQS/NodeCreation.java
+++ LinkedListQS/NodeCreation.java
@@ -1,6 +1,6 @@
 package LinkedListQS;
 
-public class NodeCreation {
+public class NodeCreation{
 public Node head;
   public Node tail;
   public int size;
@@ -19,5 +19,38 @@ public Node head;
         this.value = value;
         this.next = next;
     }
+}
+       //Just a basic Method to add data for Testing purposes
+public void InsertFirst(int val){ 
+  Node node = new Node(val);
+  node.next=head;
+  head=node;
+  if (tail==null) {
+      tail=head;
+  }
+  size++;
+}
+
+public void display(){ 
+  Node temp = head;
+  while (temp!= null) {
+    System.out.print(temp.value+"->");
+    temp=temp.next;
+    if (temp==null) {
+      System.out.print("End");
+    }
+  }
+  System.out.println();
+}
+
+public void InsertLast(int val){
+  if(tail==null){
+    InsertFirst(val); 
+    return; // Check if the list is emepty then just call the InsertFirst method
+  }
+  Node node = new Node(val);
+  tail.next= node; 
+  tail= node;
+  size++;
 }
 }
