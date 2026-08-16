614611a58678822fc663d0d02b43bc6cad1b9ce3
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-16 12:29:53 +0530

    Changed in main to work

--- LinkedListQS/Main.java
+++ LinkedListQS/Main.java
@@ -6,13 +6,19 @@ public class Main {
         // list1.InsertFirst(10);
         // list1.InsertFirst(9);
         // list1.InsertFirst(8);
-        // list1.display();  //UNCOMMENT FOR MERGE BOTH SORTED LINKEDLISTS
+        // list1.display(); //UNCOMMENT FOR MERGE BOTH SORTED LINKEDLISTS
         // MergeSorted list2 = new MergeSorted();
         // list2.InsertFirst(90);
         // list2.InsertFirst(80);
         // list2.InsertFirst(70);
         // list2.display();
         // MergeSorted.Merge(list1,list2);
-        
+        Middle obj = new Middle();
+        obj.InsertFirst(10);
+        obj.InsertFirst(90);
+        obj.InsertFirst(80);
+        obj.InsertFirst(70);
+        obj.display();
+        obj.GetMiddle();
     }
 }
