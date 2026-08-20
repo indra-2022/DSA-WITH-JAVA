cfd2052c3d9480a255f0adfa988e35c157e5df78
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-20 10:37:58 +0530

    make changes in Class name

---
+++ StackAndQueue/IntroductionToStack.java
@@ -0,0 +1,19 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class IntroductionToStack {
+   public static void main(String[] args) {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);  // In built methods for stack push
+    stack.push(40);
+    stack.push(50);
+    System.out.println(stack.toString()); //To dispplay we are comverting it to String
+      System.out.println(stack.pop()+" Is Deleted");
+      //The pop method internally returns ṭhe data its deleted
+      //  so we can just print the data using sout
+      System.out.println(stack.toString());
+   }
+}
