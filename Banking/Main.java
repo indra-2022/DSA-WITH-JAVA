f73cd35155bcdcc237f3112f25fa7f9211141b6d
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-27 12:14:56 +0530

    added comment

--- Banking/Main.java
+++ Banking/Main.java
@@ -3,6 +3,7 @@ package Banking;
 public class Main {
   public static void main(String[] args) {
     Account obj = new Account("Indranil Ganguly",0000);
+    //  Required pin for verification
     System.out.println(obj.getActNo());
     System.out.println(obj.getName());
     obj.DepositeBalance(0000, 100000000);
