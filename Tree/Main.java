54c0bd433b5c7c299044a3d2c41788bdca9e1862
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-29 12:48:04 +0530

    Change Main for BST

--- Tree/Main.java
+++ Tree/Main.java
@@ -4,9 +4,18 @@ public class Main {
     // See the hand notes for better explaination and implementation.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        TreeIntro Tree = new TreeIntro();
-        //Use true false to populate
-        Tree.populate(sc);
-        Tree.display();
-    }
+    //     TreeIntro Tree = new TreeIntro();
+    //     //Use true false to populate
+    //     Tree.populate(sc);
+    //     Tree.display();
+      BST bst = new BST();
+      bst.Insert(10);
+      bst.Insert(12);
+      bst.Insert(15);
+      bst.Insert(8);
+      bst.Insert(20);
+      System.out.println(bst.IsEmpty());
+      bst.display();
+      bst.GetHeight(null);
+     }
 }
