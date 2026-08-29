c5f9bf683b67dccf09b5cce236044ee18ee5af06
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-08-29 11:12:04 +0530

    Hsand Notes

--- Tree/Main.java
+++ Tree/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    // See the hand notes for better explaination and implementation.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TreeIntro Tree = new TreeIntro();
