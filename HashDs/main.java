d42e96cf0dbd4dcfe6c36ecbb34ec08c54dc3e82
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-09-05 14:12:58 +0530

    Normal

--- HashDs/main.java
+++ HashDs/main.java
@@ -9,15 +9,15 @@ public class main {
         map.put("Indra", 99);
         map.put("alex", 19);
         map.put("ankit", 69);
-        System.out.println(map.get("Indra"));
-        System.out.println(map.containsKey("alex"));
-        map2.put(10, "Lord Indra");
-        map2.put(10, "Lord");
-        System.out.println(map2.get(10));
-        // map2.clear();
-        System.out.println(map2.isEmpty());
-        System.out.println(map.keySet());
-        System.out.println(map2.size());
-
+        // System.out.println(map.get("Indra"));
+        // System.out.println(map.containsKey("alex"));
+        // map2.put(10, "Lord Indra");
+        // map2.put(10, "Lord");
+        // System.out.println(map2.get(10));
+        // // map2.clear();
+        // System.out.println(map2.isEmpty());
+        // System.out.println(map.keySet());
+        // System.out.println(map2.size());
+       System.out.println(map);
     }
 }
