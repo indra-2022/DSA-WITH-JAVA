55e167702baba606f4d612f1f52a96b283ec0b55
Indranil Ganguly <153382322+indra-2022@users.noreply.github.com>
2026-09-05 12:41:43 +0530

    Basics of Hashmap

---
+++ Hashmap/Intro.java
@@ -0,0 +1,12 @@
+package Hashmap;
+
+public class Intro {
+   public static void main(String[] args) {
+    String nm="Indra";
+    System.out.println(nm.hashCode());// It will convert it to a hashcode,usin some 
+    //formula, it will be the same everytime
+    Integer a=10;
+    System.out.println(a.hashCode());// Usually INTEGER hash code is the sm as the data
+    //We use INTEGER not int 
+   }
+}
---
+++ Hashmap/main.java
@@ -0,0 +1,21 @@
+package Hashmap;
+
+import java.util.HashMap;
+
+public class main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Integer, String> map2 = new HashMap<>();
+        map.put("Indra", 99);
+        map.put("alex", 19);
+        map.put("ankit", 69);
+        System.out.println(map.get("Indra"));
+        System.out.println(map.containsKey("alex"));
+map2.put(10, "Lord Indra");
+        System.out.println(map2.get(10));
+        // map2.clear();
+        System.out.println(map2.isEmpty());
+        System.out.println(map.keySet());
+
+    }
+}
